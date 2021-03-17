d5d7092f1d345b312c44b4d78243888b6495ea28
Kavya Dhar <2020a1r112@mietjammu.in>
2021-03-17 15:00:55 +0530

    Euclidean Distance   program added

---
+++ 1_Euclidean_distance.c
@@ -0,0 +1,19 @@
+#include <stdio.h> // Preprocessor to include the standard input and output funtions
+#include <math.h> // Preprocessor to include the Mathematics Funtions
+
+int main() {
+	float a, b, c, d, distance,E_distance; // variable declarztion 
+	printf("Input A: "); // taking Value of A
+	scanf("%f", &a);
+	printf("Input B: ");//taking Value of B
+	scanf("%f", &b);
+    printf("Input C: ");//taking Value of C
+	scanf("%f", &c);
+	printf("Input d: ");//taking Value of D
+	scanf("%f", &d);
+	distance = ((c-a)*(c-a))+((d-b)*(d-b));// logic 
+	E_distance=sqrt(distance);// applying Sqrt 
+	printf("Distance between the said points: %f",E_distance); // printing output
+	printf("\n");
+	return 0;
+}
