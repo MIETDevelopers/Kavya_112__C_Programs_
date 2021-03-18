ba9f47b0d2778f9e51f64f4fbf7135e2b46142c4
Kavya Dha <76278131+Kavyaa1r112@users.noreply.github.com>
2021-03-18 23:00:37 +0530

    Update Addition_array.c

--- Addition_array.c
+++ Addition_array.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
-#include <conio.h>
-void main()
+int main()
 {
-	int a[2][3],b[2][3],c[2][3],i,j;
-	printf("\nENTER VALUES FOR MATRIX A:\n");
-	for(i=0;i<2;i++)
-		for(j=0;j<3;j++)
-			scanf("%d",&a[i][j]);
-	printf("\nENTER VALUES FOR MATRIX B:\n");
-	for(i=0;i<2;i++)
-		for(j=0;j<3;j++)
-			scanf("%d",&b[i][j]);
-	for(i=0;i<2;i++)
-		for(j=0;j<3;j++)
-			c[i][j]=a[i][j]+b[i][j];
-	printf("\nTHE VALUES OF MATRIX C ARE:\n");
+	int a[2][3],b[2][3],c[2][3],i,j; // Declaring Variables 
+	printf("\nENTER VALUES FOR MATRIX A:\n"); // taking Matrix A elemnts
+	for(i=0;i<2;i++) // for loop 
+		for(j=0;j<3;j++) // for loop 
+			scanf("%d",&a[i][j]); // taking input 
+	printf("\nENTER VALUES FOR MATRIX B:\n");  // taking Matrix B elemnts 
+	for(i=0;i<2;i++)// for loop 
+		for(j=0;j<3;j++)// for loop 
+			scanf("%d",&b[i][j]);// taking input 
+	for(i=0;i<2;i++)// for loop 
+		for(j=0;j<3;j++)// for loop 
+			c[i][j]=a[i][j]+b[i][j]; //Logic 
+	printf("\nTHE VALUES OF MATRIX C ARE:\n"); // Prinitng the Matrix C 
 	for(i=0;i<2;i++)
         {
-		for(j=0;j<3;j++)
-			printf("%5d",c[i][j]);
+		for(j=0;j<3;j++) //for loop 
+			printf("%5d",c[i][j]); // Printing the matrix 
 		printf("\n");
 	}
-	getch();
+	return 0;
 }
