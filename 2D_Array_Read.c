d74071e6f5996e7a1e325e3d20d123940b2908cb
Kavya Dhar <2020a1r112@mietjammu.in>
2021-03-18 22:25:10 +0530

    To read and print elements in two dimensional array program added

---
+++ 2D_Array_Read.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main()
+{
+  // array declaration and initialization
+  int my_array[5][3] = {
+    {1, 2, 3},      //row 1
+    {4, 5, 6},      //row 2
+    {7, 8, 9},      //row 3
+    {10, 11, 12},   //row 4
+    {13, 14, 15}    //row 5
+  };
+
+  // accessing and printing the elements
+  for ( int i = 0; i < 5; i++ ) {   
+      // variable i traverses the rows
+      for ( int j = 0; j < 3; j++ ) {
+         // variable j traverses the columns
+         printf("my_array [%d][%d] = %d\n", i,j, my_array[i][j] );
+      }
+   }
+   return 0;
+}
