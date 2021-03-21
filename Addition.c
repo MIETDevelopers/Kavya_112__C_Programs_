892a17f9b155ed956eb48bd06b7c4c67bd965759
Kavya Dha <76278131+Kavyaa1r112@users.noreply.github.com>
2021-03-21 21:26:31 +0530

    Update Addition.c

--- Addition.c
+++ Addition.c
@@ -1,14 +1,14 @@
 #include <stdio.h> // Preprocessive to include standard input and output 
-int main() {    
+int main() { // Main Body   
 
-    int num1, num2, sum;
+    int num1, num2, sum; // Declaring Variable
     
     printf("Enter two integers: "); // taking input 
     scanf("%d %d", &num1, &num2); // Scaning output
 
     // calculating sum
-    sum = num1 + num2;      
+    sum = num1 + num2;    // Logic  
     
     printf("%d + %d = %d", num1, num2, sum); // Priniting output
     return 0;
-}
+}     // End of Body
