0ed8b9cedfac6d20955a1ab8c3785be2cd75585b
Kavya Dha <76278131+Kavyaa1r112@users.noreply.github.com>
2021-03-21 22:04:25 +0530

    Update 1_Hello.c

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,3 +1,6 @@
+//  Author Name : Kavya Dhar
+// Creation date: 17-03-21
+// Purpose:To Print Hello World 
 #include<stdio.h> // Pre-Processive to include standard input and output header files funtions 
 int main() // Start of body
 {
