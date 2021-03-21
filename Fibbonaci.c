eb24703fc9ca5ab57f0f942c241f1fa39e4849de
Kavya Dha <76278131+Kavyaa1r112@users.noreply.github.com>
2021-03-21 23:10:19 +0530

    Update Fibbonaci.c

--- Fibbonaci.c
+++ Fibbonaci.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
+//  Author Name : Kavya Dhar
+// Creation date: 18-03-21
+// Purpose: To Find the Factorial of a Program 
 int main() {
-    int i, n, t1 = 0, t2 = 1, nextTerm;
-    printf("Enter the number of terms: ");
+    int i, n, t1 = 0, t2 = 1, nextTerm; // Variable Declration 
+    printf("Enter the number of terms: "); // Taking Input from user
     scanf("%d", &n);
-    printf("Fibonacci Series: ");
+    printf("Fibonacci Series: "); 
 
     for (i = 1; i <= n; ++i) {
         printf("%d, ", t1);
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
+        nextTerm = t1 + t2; // Logic
+        t1 = t2; // Logic 
+        t2 = nextTerm; // Logic
     }
 
     return 0;
