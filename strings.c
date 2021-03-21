00daac81ee61b0d2d7d29bcd53b3916483ffb1ec
Kavya Dha <76278131+Kavyaa1r112@users.noreply.github.com>
2021-03-21 23:44:36 +0530

    Update strings.c

--- strings.c
+++ strings.c
@@ -12,7 +12,7 @@ strncmp - compare parts of two strings
 strncpy - copy part of a string
 strrchr - string scanning operation */
 
-// C String function – strlen
+// C String function â€“ strlen
 #include <stdio.h>
 #include <string.h>
 int main()
@@ -22,7 +22,7 @@ int main()
      printf("Length of string str1 when maxlen is 10: %d", strnlen(str1, 10));
      return 0;
 }
-// C String function – strcmp
+// C String function â€“ strcmp
 #include <stdio.h>
 #include <string.h>
 int main()
@@ -38,7 +38,7 @@ int main()
       }
      return 0;
 }
-//C String function – strcat
+//C String function â€“ strcat
 #include <stdio.h>
 #include <string.h>
 int main()
@@ -49,33 +49,33 @@ int main()
      printf("Output string after concatenation: %s", s1);
      return 0;
 }
-//C String function – strcpy
+//C String function â€“ strcpy
 #include <stdio.h>
 #include <string.h>
 int main()
 {
      char s1[30] = "string 1";
-     char s2[30] = "string 2 : I’m gonna copied into s1";
+     char s2[30] = "string 2 : Iâ€™m gonna copied into s1";
      /* this function has copied s2 into s1*/
      strcpy(s1,s2);
      printf("String s1 is: %s", s1);
      return 0;
 }
-//C String function – strchr
+//C String function â€“ strchr
 #include <stdio.h>
 #include <string.h>
 int main()
 {
-     char mystr[30] = "I’m an example of function strchr";
+     char mystr[30] = "Iâ€™m an example of function strchr";
      printf ("%s", strchr(mystr, 'f'));
      return 0;
 }
-//C String function – strstr
+//C String function â€“ strstr
 #include <stdio.h>
 #include <string.h>
 int main()
 {
      char inputstr[70] = "String Function in C at Kavya Dhar ";
-     printf ("Output string is: %s", strstr(inputstr, 'Begi'));
+     printf ("Output string is: %s", strstr(inputstr, 'Kavy'));
      return 0;
 }
