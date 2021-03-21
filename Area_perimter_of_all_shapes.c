4dc2e33fc1888999c83c6267e5600c22abce51d5
Kavya Dha <76278131+Kavyaa1r112@users.noreply.github.com>
2021-03-21 21:36:58 +0530

    Update Area_perimter_of_all_shapes.c

--- Area_perimter_of_all_shapes.c
+++ Area_perimter_of_all_shapes.c
@@ -1,12 +1,12 @@
 #include <stdio.h> // Pre-Processive to include standard input and output header file funtions 
-int main() // Start of body 
+int main() // Start of Main body 
 {
-    float side, area_Square, area_Circle,radius,length,breadth,area_Rectangle,perimeter_Rectangle,circumfernce,perimeter_Square;
+    float side, area_Square, area_Circle,radius,length,breadth,area_Rectangle,perimeter_Rectangle,circumfernce,perimeter_Square; // Variable Declaration 
     printf("Enter  of side of square\n");// Read the statment
     scanf("%f", &side);// output statment
     area_Square = side * side; // logic (formula) 
    perimeter_Square=4*side; // logic (formula)
-	 printf("\nEnter the radius of Circle : ");
+	 printf("\nEnter the radius of Circle : "); // Radius 
 	scanf("%f", &radius);
 	area_Circle = 3.14 * radius * radius;// logic (formula)
 	circumfernce=2*(3.14*radius);// logic (formula)
