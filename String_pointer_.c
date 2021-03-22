896c69b14e53c0de9e8bf8d3e71c3e3f009e2ab6
Kavya Dhar <2020a1r112@mietjammu.in>
2021-03-22 23:24:32 +0530

    String Pointer added

---
+++ String_pointer_.c
@@ -0,0 +1,18 @@
+//A C Program to read the string in pointers loaction and give it as output.(Pre-Defined) 			© ® Kavya 21/March/2021
+#include <stdio.h>
+
+int main(void) {
+  char name[] = "Kavya Dhar";
+
+  printf("%c \n", *name);     	// Output will be K
+  printf("%c \n", *(name+3));	// Output will be  y
+  printf("%c \n", *(name+8));   // Output will be a 
+
+  char *namePtr;
+
+  namePtr = name;
+  printf("%c \n", *namePtr);     // Output will be K
+  printf("%c \n", *(namePtr+2)); // Output will be V
+  printf("%c \n", *(namePtr+7)); // Output will be h
+}
+ 
