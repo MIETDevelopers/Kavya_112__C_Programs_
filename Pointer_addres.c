0c9d828eeb6d4717b386255dbf850a2bb437a2fc
Kavya Dhar <2020a1r112@mietjammu.in>
2021-03-23 16:39:43 +0530

    Programs added

---
+++ Pointer_addres.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;  
+    a = 10;
+    int *p = &a;     // declaring and initializing the pointer
+
+    //prints the value of 'a'
+    printf("%d\n", *p);  
+    printf("%d\n", *&a);  
+    //prints the address of 'a'
+    printf("%u\n", &a);    
+    printf("%u\n", p);     
+    
+    printf("%u\n", &p);    //prints address of 'p'
+    
+    return 0;
+}
