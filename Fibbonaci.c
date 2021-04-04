9217631887f3979ff1773faf95d3fbd25bf6c41b
Kavya Dhar <76278131+Kavyaa1r112@users.noreply.github.com>
2021-04-04 20:15:41 +0530

    Update Fibbonaci.c

--- Fibbonaci.c
+++ Fibbonaci.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-int main() {
-    int i, n, t1 = 0, t2 = 1, nextTerm;
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-    printf("Fibonacci Series: ");
+#include <stdio.h> // Pre-Processive directory standard library to include input and output 
+int main() { //  Start of Main body
+    int i, n, t1 = 0, t2 = 1, nextTerm;     // Variable Declaration  
+    printf("Enter the number of terms: "); //Printing the number of terms
+    scanf("%d", &n); 
+    printf("Fibonacci Series: "); // Printing Fibonacci Series
 
-    for (i = 1; i <= n; ++i) {
-        printf("%d, ", t1);
+    for (i = 1; i <= n; ++i) { // Begining of for loop
+        printf("%d, ", t1); 
         nextTerm = t1 + t2;
         t1 = t2;
         t2 = nextTerm;
     }
 
-    return 0;
+    return 0; // End of Body 
 }
