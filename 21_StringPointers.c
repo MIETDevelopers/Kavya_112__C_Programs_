5926183f321f84a55355e0cb1a378ba2c03da2ef
Kavya Dhar <76278131+Kavyaa1r112@users.noreply.github.com>
2021-04-07 09:06:36 +0530

    C Program files added

--- 21_StringPointers.c
+++ 21_StringPointers.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 int main(void) {
-  char name[] = "Ishav Verma";
+  char name[] = "Kavya Dhar";
 
-  printf("%c \n", *name);     	// Output will be I
-  printf("%c \n", *(name+3));	// Output will be A
-  printf("%c \n", *(name+8));   // Output will be R
+  printf("%c \n", *name);     	// Output will be K
+  printf("%c \n", *(name+3));	// Output will be y
+  printf("%c \n", *(name+8));   // Output will be a
 
   char *namePtr;
 
   namePtr = name;
-  printf("%c \n", *namePtr);     // Output will be I
-  printf("%c \n", *(namePtr+2)); // Output will be H
-  printf("%c \n", *(namePtr+7)); // Output will be E
+  printf("%c \n", *namePtr);     // Output will be K
+  printf("%c \n", *(namePtr+2)); // Output will be V
+  printf("%c \n", *(namePtr+7)); // Output will be h
 }
