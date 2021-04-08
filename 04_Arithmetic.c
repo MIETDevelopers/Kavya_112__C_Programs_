5337e20df862f9ac8eb1730ab7986910a3be7991
Kavya Dhar <76278131+Kavyaa1r112@users.noreply.github.com>
2021-04-08 13:43:50 +0530

    C Program added

--- 04_Arithmetic.c
+++ 04_Arithmetic.c
@@ -1,29 +1,33 @@
-#include<stdio.h> 
-int main(){
-    char operator;
-    double a, b;
-    printf("Enter an operator (+, -, *, /):"); //It will print the operater to be entered by user.
-    scanf("%c", &operator); //It will scan the operater entered by user.
-    printf("Enter two operands:");
-    scanf("%lf %lf",&a, &b); //It will scan the numbers entered by user.
-    switch(operator)
+#include<stdio.h>    // Pre-Prossive to include standard input and out put header files funtions
+#include<conio.h>    // Pre-Prossive to include console  input and out put header files funtions
+int main()
+// Start of main body
+{
+    int a,b; // Variable Declaratation
+    int op; //  Variable Declaratation
+    printf(" 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n"); // Printing Statement
+    printf("Enter the values of a & b: "); // Taking Input from user
+    scanf("%d %d",&a,&b);
+    printf("Enter your Choice : "); // Choice from User
+    scanf("%d",&op); // 
+    switch(op)
     {
-        case '+':
-            printf("%.2lf + %.2lf = %.2lf",a, b, a+b); //Addition Function
-            break;
-
-        case '-':
-            printf("%.2lf - %.2lf = %.2lf",a, b, a-b); //Subtraction Function
-            break;
-
-        case '*':
-            printf("%.2lf * %.2lf = %.2lf",a, b, a*b); //Multiplication Function
-            break;
-
-        case '/':
-            printf("%.2lf / %.2lf = %.2lf",a, b, a/b); //Division Function
-            break;
-    printf("Error encountered by the input values");
+    case 1	:
+        printf("Sum of %d and %d is : %d",a,b,a+b);
+        break;
+    case 2	:
+        printf("Difference of %d and %d is : %d",a,b,a-b);
+        break;
+    case 3	:
+        printf("Multiplication of %d and %d is : %d",a,b,a*b);
+        break;
+    case 4	:
+        printf("Division of Two Numbers is %d : ",a/b);
+        break;
+    default	:
+        printf(" Enter Your Correct Choice.");
+        break;
     }
     return 0;
 }
+// End of Body
