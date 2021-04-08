28704f8df365d816b92267add02a25712c1774ec
Kavya Dhar <76278131+Kavyaa1r112@users.noreply.github.com>
2021-04-08 22:13:32 +0530

    C Program File added

--- 15_Armstrong.c
+++ 15_Armstrong.c
@@ -1,7 +1,7 @@
 //Kavya Dhar 20/March/2021
-#include <stdio.h>
+#include <stdio.h>  // Pre-Processive to include standard input and output header files funtions
 int main() {
-  int num, originalNum, remainder, result = 0;
+  int num, originalNum, remainder, result = 0;   // Variable Delrartation
   printf("Enter a three-digit integer: \n");
   scanf("%d", &num);//It will scan the operater entered by user.
   originalNum = num;
