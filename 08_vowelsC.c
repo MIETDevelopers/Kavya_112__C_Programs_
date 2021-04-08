ad884c9115fe05ffbf293e07843db34da33f09b3
Kavya Dhar <76278131+Kavyaa1r112@users.noreply.github.com>
2021-04-08 17:33:05 +0530

    Update 08_vowelsC.c

--- 08_vowelsC.c
+++ 08_vowelsC.c
@@ -2,22 +2,49 @@
 // Creation Date:22-03-21
 // Purpose:To Check the alphabet is vowel or consonant
 #include <stdio.h>
-int main() {
-    char c;
-    int lowercase_vowel, uppercase_vowel;
+#include<conio.h>
+int main()
+ {
+    char v;
     printf("Enter an alphabet: ");
-    scanf("%c", &c);
+    scanf("%c", &v);
 
-    // evaluates to 1 if variable c is a lowercase vowel
-    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-
-    // evaluates to 1 if variable c is a uppercase vowel
-    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-
-    // evaluates to 1 (true) if c is a vowel
-    if (lowercase_vowel || uppercase_vowel)
-        printf("%c is a vowel.", c);
-    else
-        printf("%c is a consonant.", c);
-    return 0;
+    // evaluates to 1 if variable c is a  vowel
+    switch(v)
+	
+{
+	case 'a':
+		printf("Yes its a vowel","\n",v);
+		break;
+	case 'e':
+		printf("Yes its a vowel","\n",v);
+		break;
+	case 'i':
+		printf("yes its a vowel","\n",v);
+		break;
+	case 'o':
+		printf("Yes its a vowel","\n",v);
+				break;
+	case 'u':
+		printf("Yes its a vowel","\n",v);				
+		 break;
+	case 'A':
+		printf("Yes its a vowel","\n",v);
+		break;
+	case 'E':
+		printf("Yes its a vowel","\n",v);
+		break;
+	case 'I':
+		printf("yes its a vowel","\n",v);
+		break;
+	case 'O':
+		printf("Yes its a vowel","\n",v);
+				break;
+	case 'U':
+		printf("Yes its a vowel","\n",v);				
+		 break;
+	default :
+		printf("Oops Its Constant","\n",v)	; 
+	}
 }
+	
