d54a17b010675f9b765c84085d1144c71f905f3c
Kavya Dhar <76278131+Kavyaa1r112@users.noreply.github.com>
2021-04-11 21:09:03 +0530

    Array as Arguments

--- array_arguments.c
+++ array_arguments.c
@@ -1,27 +1,44 @@
 // Author : Kavya Dhar
 // Creation Date : 23-03-21 
 //  Purpose: 1D and 2D arrays passed as arguments to a single function and the function implementation should simply print those arrays passed as arguments.
-#include <stdio.h>
-void displayNumbers(int num[2][2]);
-int main()
-{
-    int num[2][2],i,j;
-    printf("Enter 4 numbers:\n");
-    for (i = 0; i < 2; ++i)
-        for (j = 0; j < 2; ++j)
-            scanf("%d", &num[i][j]);
-
-    // passing multi-dimensional array to a function
-    displayNumbers(num);
-    return 0;
+/Purpose: Passing arrays as arguments to a function.
+#include<stdio.h>	//Preprocessor directive to include standard input output header file
+//Start of the main body function
+void printArrays(int arr1[],int arr2[][3] );
+int main() {
+ int a,b,i,j;
+    int _1Array[5]; //declaring a 2D array with 2 rows and 3 columns.
+    int _2Array[2][3]; //declaring a 2D array with 2 rows and 3 columns.
+	printf("Enter values for 1st Array\n");
+    for(i=0;i<5;i++) {  //This for loop determines the value of rows.
+            printf("Index [%d]: ",i);
+            scanf("%d",&_1Array[i]);
+        }
+    printf("\nEnter values for 2nd Array\n");
+    for( i=0;i<2;i++) {  //This for loop determines the value of rows.
+        for( j=0;j<3;j++) {  //This for loop determines the value of columns.
+            printf("Index [%d] [%d]: ",i,j);
+            scanf("%d",&_2Array[i][j]);
+        }
+    }
+    printArrays(_1Array,_2Array);
+	return 0;	//return statement
 }
-
-void displayNumbers(int num[2][2])
-{
-    printf("Displaying:\n");
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-           printf("%d\n", num[i][j]);
+//End of the main body function
+int i,j;
+void printArrays(int arr1[],int arr2[][3]) {
+	//1st array.
+    printf("\nPrinting 1st array\n");
+    for( i=0;i<5;i++) {  //This for loop determines the value of rows.
+            printf("%d ",arr1[i]);
+        }
+    //2nd array.
+    printf("\nPrinting 2nd array\n");
+    for( i=0;i<2;i++) {  //This for loop determines the value of rows.
+        for( j=0;j<3;j++) {  //This for loop determines the value of columns.
+            printf("%d ",arr2[i][j]);
+            if(j==2)
+                printf("\n");
         }
     }
 }
