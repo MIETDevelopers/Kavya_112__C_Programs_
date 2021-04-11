b1535de248406b9018042c28f916ac72aa3d9402
Kavya Dhar <76278131+Kavyaa1r112@users.noreply.github.com>
2021-04-11 20:44:39 +0530

    Sum of array

--- Sumof_array.c
+++ Sumof_array.c
@@ -1,28 +1,27 @@
 // Author : Kavya Dhar
 //  Creation Date: 22-03-21
 //  Purpose: sum of elements of an array with 10 integer elements. 
-#include<stdio.h>
-#include <conio.h>
- int main()
-{
-    int a[10],i,n,sum=0;
-   
-    printf("Enter size of the array : ");
-    scanf("%d",&n);
- 
-    printf("Enter elements in array : ");
-    for(i=0; i<n; i++)
-    {
-        scanf("%d",&a[i]);
+//Purpose: Sum of an array with 10 elements.
+#include <stdio.h>	//Preprocessor directive to include standard input output header file
+//Start of the main body function
+int main() {
+
+   int sum=0; // Variable declaratation
+
+    int numbers[10]; //declaration of an array.
+
+    //for loop to insert values to the array.
+    for(int i=0;i<10;i++) {
+        printf("Enter element no. %d: ",i+1);
+        scanf("%d",&numbers[i]);
     }
- 
-    
-    for(i=0; i<n; i++)
-    {
-         
-        sum+=a[i];
+
+    //for loop to print the entire array.
+    for(int i=0;i<10;i++) {
+        sum = sum + numbers[i];
     }
-     printf("sum of array is : %d",sum);
- 
-    return 0;
+    printf("\nSum of all elements = %d",sum);
+
+	return 0;	//return statement
 }
+//End of the main body function
